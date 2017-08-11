067904155a7026484ec9d5694b3c07736c6274d9
Piotrek <szarlej700@wp.pl>
2017-08-11 15:27:46 +0200

    add formulage

---
+++ app/app.component.js
@@ -0,0 +1,45 @@
+System.register(['angular2/core', './article'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, article_1;
+    var AppComponent, ARTICLES;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (article_1_1) {
+                article_1 = article_1_1;
+            }],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'articles',
+                        template: ""
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
+            }());
+            exports_1("AppComponent", AppComponent);
+            ARTICLES = [
+                new article_1.Article('Semantic UI', 'http://semantic-ui.com/images/logo.png', 'Semantic empowers designers and developers by creating a shared vocabulary for UI.', 'http://semantic-ui.com/images/logo.png', 0),
+                new article_1.Article('Angular 2', 'http://semantic-ui.com/images/logo.png', 'Headers may be oriented to give the hierarchy of a section in the context of the page.', 'resources/images/ang.png', 0),
+                new article_1.Article('Angular 2', 'http://semantic-ui.com/images/logo.png', 'Headers may be oriented to give the hierarchy of a section in the context of the page.', 'resources/images/ang.png', 0),
+            ];
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,17 @@
 import {Component} from 'angular2/core';
+import {NgFor} from 'angular2/common';
+import {Article} from './article';
+
 @Component({
 	selector: 'articles',
-	template: '<div>To jest komponent</div>'
+	template: ``
 })
 export class AppComponent{
 
 }
+var ARTICLES: Article[] = [
+new Article('Semantic UI', 'http://semantic-ui.com/images/logo.png', 'Semantic empowers designers and developers by creating a shared vocabulary for UI.', 'http://semantic-ui.com/images/logo.png', 0),
+new Article('Angular 2', 'http://semantic-ui.com/images/logo.png', 'Headers may be oriented to give the hierarchy of a section in the context of the page.', 'resources/images/ang.png', 0),
+new Article('Angular 2', 'http://semantic-ui.com/images/logo.png', 'Headers may be oriented to give the hierarchy of a section in the context of the page.', 'resources/images/ang.png', 0),
+];
+
